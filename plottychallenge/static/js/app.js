bf5a898cb6e75dd5e195aca0f67055a41ea79210
Gustavo Mendoza <gmendoza@Gustavos-MacBook-Pro.local>
2019-11-01 19:36:12 -0500

    files

---
+++ plottychallenge/static/js/app.js
@@ -0,0 +1,118 @@
+function buildMetadata(sample) {
+  // @TODO: Complete the following function that builds the metadata panel
+  
+  var url =`/metadata/${sample}`;
+  // Use `d3.json` to fetch the metadata for a sample
+  
+  d3.json(url).then(function(sample){
+    console.log(sample);
+
+  // Use d3 to select the panel with id of `#sample-metadata`
+  
+  var sample_metadata = d3.select("#sample-metadata");
+
+    // Use `.html("") to clear any existing metadata
+  sample_metadata.html("");
+    // Use `Object.entries` to add each key and value pair to the panel
+    // Hint: Inside the loop, you will need to use d3 to append new
+    // tags for each key-value in the metadata.
+
+  Object.entries(sample).forEach(function([key,value]) {
+    var row = sample_metadata.append("p");
+    row.text(`${key} : ${value}`)
+
+    });
+  }
+)};
+
+
+
+    // BONUS: Build the Gauge Chart
+    // buildGauge(data.WFREQ);
+function buildCharts(sample) {
+    var url= `/samples/${sample}`
+
+  // @TODO: Use `d3.json` to fetch the sample data for the plots
+    d3.json(url).then(function(data){
+
+    // @TODO: Build a Bubble Chart using the sample data
+    var x_values = data.otu_ids;
+    var y_values = data.sample_values;
+    var m_size = data.sample_values;
+    var m_color = data.otu_ids;
+    var t_values = data.otu_labels;
+    
+    var trace_1 = {
+      x: x_values,
+      y: y_values, 
+      text: t_values,
+      mode: 'markers',
+      marker:{
+        color: m_color ,
+        size: m_size,
+      }
+    };
+    
+    var data = [trace_1]
+    var layout = {
+      xaxis : {title: "OTU ID"}
+    };
+
+    Plotly.newPlot('bubble',data,layout)
+
+
+    // @TODO: Build a Pie Chart
+    // HINT: You will need to use slice() to grab the top 10 sample_values,
+    // otu_ids, and labels (10 each).
+
+    d3.json(url).then(function(data) {
+      var pie_chart_values = data.sample_values.slice(0,10); 
+      var pie_chart_labels = data.otu_ids.slice(0,10) ;
+      var pie_chart_hoover = data.otu_labels.slice(0,10) ;
+
+      var data = [{
+        values : pie_chart_values,
+        labels : pie_chart_labels,
+        hovertext: pie_chart_hoover,
+        type: 'pie',
+      }];
+      
+      Plotly.newPlot('pie',data)
+
+ 
+    });
+    });
+  }
+  
+
+
+
+function init() {
+  // Grab a reference to the dropdown select element
+  var selector = d3.select("#selDataset");
+
+  // Use the list of sample names to populate the select options
+  d3.json("/names").then((sampleNames) => {
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
+
+    // Use the first sample from the list to build the initial plots
+    const firstSample = sampleNames[0];
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
+  });
+}
+
+
+function optionChanged(newSample) {
+  // Fetch new data each time a new sample is selected
+  buildCharts(newSample);
+  buildMetadata(newSample);
+}
+
+// Initialize the dashboard
+init();
---
+++ plottychallenge/static/js/bonus.js
@@ -0,0 +1,24 @@
+function buildGauge(wfreq) {
+
+
+var data = [
+    {
+      type: "scatter",
+      x: [0],
+      y: [0],
+      marker: { size: 12, color: "850000" },
+      showlegend: false,
+      name: "Freq",
+      text: level,
+      hoverinfo: "text+name"
+    },
+    {
+      labels: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+      hoverinfo: "label",
+      hole: 0.5,
+      type: "pie",
+      showlegend: false
+    }
+  ];
+
+}
